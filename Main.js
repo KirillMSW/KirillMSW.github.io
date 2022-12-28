a0a8db9c863dde350810c52ced955448ebaf696c
kirilltsybrov <kzybrow@yandex.ru>
2022-12-29 00:16:12 +0300

    Initial commit

---
+++ Main.js
@@ -0,0 +1,199 @@
+let planets = []
+planetNum = 2
+aimMode = true
+targetNum=1
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    frameRate(60);
+    let cue = new Planet(Math.random() * window.innerWidth, 
+                            Math.random() * window.innerHeight,
+                            300, false,0)
+    planets.push(cue)
+
+    let regular = new Planet(Math.random() * window.innerWidth, 
+                            Math.random() * window.innerHeight,
+                            300, false,1)
+    planets.push(regular)
+
+    for (let i = 0; i < targetNum; ++i){
+        let pl = new Planet(Math.random() * window.innerWidth, 
+        Math.random() * window.innerHeight,
+        300, false,3)
+        planetNum++
+        planets.push(pl)
+    }
+   
+    while (are_coliding())
+        generate()
+
+
+    planets[0].velocity=createVector(5, 5)
+
+    var button_w=window.innerWidth*0.05
+    var button_h=window.innerHeight*0.05
+
+
+    plus_button = createButton('+');
+    plus_button.position((window.innerWidth ) / 2-2*button_w, window.innerHeight-button_h);
+    plus_button.style('color', 'white')
+    plus_button.style('background', '#1a1b26')
+    plus_button.size(button_w,button_h)
+    plus_button.mousePressed(plus);
+
+    minus_button = createButton('-');
+    minus_button.position((window.innerWidth ) / 2-button_w, window.innerHeight-button_h);
+    minus_button.style('color', 'white')
+    minus_button.style('background', '#1a1b26')
+    minus_button.size(button_w,button_h)
+    minus_button.mousePressed(minus);
+
+    shot_button = createButton('Пуск');
+    shot_button.position((window.innerWidth ) / 2, window.innerHeight-button_h);
+    shot_button.style('color', 'white')
+    shot_button.style('background', '#1a1b26')
+    shot_button.size(button_w,button_h)
+    shot_button.mousePressed(shot);
+
+    left_button = createButton('↺');
+    left_button.position((window.innerWidth ) / 2+button_w, window.innerHeight-button_h);
+    left_button.style('color', 'white')
+    left_button.style('background', '#1a1b26')
+    left_button.size(button_w,button_h)
+    left_button.mousePressed(left);
+
+    right_button = createButton('↻');
+    right_button.position((window.innerWidth ) / 2+2*button_w, window.innerHeight-button_h);
+    right_button.style('color', 'white')
+    right_button.style('background', '#1a1b26')
+    right_button.size(button_w,button_h)
+    right_button.mousePressed(right);
+
+}
+
+
+function draw() {
+    const dt = 1 / frameRate() * 60
+    angleMode(DEGREES);
+    background("#1a1b26")
+    var all_hit=true
+    for (let i = 0; i < planetNum; ++i){
+        for (let j = i + 1; j < planetNum; ++j){
+            Planet.react(planets[i], planets[j])
+        }
+        Planet.wallCollide(planets[i])
+        if (planets[i].type==3)
+            all_hit=false
+    }
+    if (all_hit){
+        are_moving=false
+        for (let i = 0; i < planetNum; ++i){
+            planets[i].move=false
+        }
+        alert("Вы выиграли!")
+        generate()
+        aimMode=true
+    }
+    
+    var are_moving=false
+    for (let i = planetNum-1; i >=0; --i){
+        planets[i].update(dt)
+        planets[i].render()
+        if (planets[i].velocity.mag()>=0.1){
+            are_moving=true
+        }
+
+    }
+    if(!(aimMode)){
+        if(!(are_moving)){
+            aimMode=true
+            for (let i = 0; i < planetNum; ++i){
+                planets[i].move=false
+            }
+            planets[0].velocity=createVector(5, 5)
+        }
+    }
+   
+    
+}
+
+
+function are_coliding() {
+    for (let i = 0; i < planetNum; ++i){
+        for (let j = i + 1; j < planetNum; ++j){
+            var r = planets[i].position.dist(planets[j].position)
+            if (r < planets[i].radius + planets[j].radius){
+                console.log(i,j)
+                return true
+
+            }
+        }
+    }
+    return false
+    
+}
+
+function generate() {
+    planets=[]
+    planetNum = 2
+
+    let cue = new Planet(Math.random() * window.innerWidth, 
+                            Math.random() * window.innerHeight,
+                            300, false,0)
+    planets.push(cue)
+
+    let regular = new Planet(Math.random() * window.innerWidth, 
+                            Math.random() * window.innerHeight,
+                            300, false,1)
+    planets.push(regular)
+
+    for (let i = 0; i < targetNum; ++i){
+        let pl = new Planet(Math.random() * window.innerWidth, 
+        Math.random() * window.innerHeight,
+        300, false,3)
+        planetNum++
+        planets.push(pl)
+    }
+   
+    
+    planets[0].velocity=createVector(5, 5)
+    
+}
+
+
+
+function plus() {
+    if (aimMode){
+        const magnitude = planets[0].velocity.mag()
+        planets[0].velocity.setMag(magnitude+2)
+    }
+}
+
+function minus() {
+    if (aimMode){
+        const magnitude = planets[0].velocity.mag()
+        if (magnitude>5)
+            planets[0].velocity.setMag(magnitude-2)
+    }
+}
+
+function shot() {
+    if (aimMode){
+        aimMode=false
+        for (let i = 0; i < planetNum; ++i){
+            planets[i].move=true
+        }
+    }
+}
+
+function left() {
+    if (aimMode){
+    planets[0].velocity.rotate(-5)
+    }
+}
+
+function right() {
+    if (aimMode){
+    planets[0].velocity.rotate(5)
+    }
+}
---
+++ Planet.js
@@ -0,0 +1,120 @@
+class Planet{
+    constructor(x=0, y=0, m=100,mv,type) {
+        this.move = mv
+        this.mass = m
+        this.acceleration = createVector(0, 0)
+        this.velocity = createVector(0, 0)
+        this.position = createVector(x, y)
+        this.radius = m / 10
+        this.g = 6
+        this.k = 1
+        this.type = type
+        if (type==0)
+            this.color = color("#FFFFFF")
+        else if (type==1)
+            this.color = color("#000000")
+        else{
+            this.color = color("#FF0000")
+            this.radius*=3
+        }
+        
+
+    }
+
+    update(dt){
+        const zero_vector=createVector(0, 0)
+        this.acceleration.mult(dt)
+        this.velocity.add(this.acceleration)
+        this.acceleration = createVector(0, 0)
+        const dPos = this.velocity.copy().mult(dt)
+        if (this.move)
+            this.position.add(dPos)
+        if ((this.move)){
+            const magnitude = this.velocity.mag()
+            this.velocity.setMag(magnitude-0.01)
+        }
+    }
+
+    applyForce(force){
+        let fc = force.copy()
+        fc.div(this.mass)
+        this.acceleration.add(fc)
+    }
+
+    render(){
+        fill(this.color)
+        noStroke()
+        circle(this.position.x, this.position.y, this.radius * 2)
+        stroke(color(255, 0, 0));
+        strokeWeight(4);
+
+        const dPos=this.position.copy().add(this.velocity.copy().mult(5))
+        line(this.position.x,this.position.y, dPos.x,dPos.y)
+    }
+
+    static react(pl1, pl2){
+        const r = pl1.position.dist(pl2.position)
+        if (r < pl1.radius + pl2.radius){
+            if ((pl1.type==3)&&(pl2.type==1)){
+                pl1.type=4
+                pl1.color="#008000"
+            }
+            if ((pl2.type==3)&&(pl1.type==1)){
+                pl2.type=4
+                pl2.color="#008000"
+            }
+            if ((pl1.type<3)&&(pl2.type<3))
+                this.collide(pl1, pl2)
+        }
+            
+        
+    }
+
+    static vectorProjection(a, b){
+        let bCopy = b.copy().normalize()
+        const sp = a.dot(bCopy)
+        bCopy.mult(sp)
+        return bCopy
+    }
+
+    static collide(pl1, pl2){
+        const tmpDir12 = createVector(pl2.position.x - pl1.position.x, pl2.position.y - pl1.position.y)
+        const offset = tmpDir12.mag() - (pl1.radius + pl2.radius)
+        pl1.position.add(tmpDir12.copy().normalize().mult(offset / 2))
+        pl2.position.add(tmpDir12.copy().normalize().mult(-offset / 2))
+        const direction12 = createVector(pl2.position.x - pl1.position.x, pl2.position.y - pl1.position.y)
+        const direction21 = createVector(pl1.position.x - pl2.position.x, pl1.position.y - pl2.position.y)
+        const pl1ImpFrac = pl2.mass / (pl1.mass + pl2.mass)
+        const pl2ImpFrac = pl1.mass / (pl1.mass + pl2.mass)
+        const impulse1Dir = this.vectorProjection(pl1.velocity, direction12).mult(pl1.mass)
+        const impulse2Dir = this.vectorProjection(pl2.velocity, direction21).mult(pl2.mass)
+        const totalImpulse = impulse1Dir.copy().add(impulse2Dir.copy().mult(-1))
+        const resImpulse1 = totalImpulse.copy().mult(-pl1ImpFrac).mult(pl1.k * pl2.k)
+        const resImpulse2 = totalImpulse.copy().mult(pl2ImpFrac).mult(pl1.k * pl2.k)
+        const stop1Force = this.vectorProjection(pl1.velocity, direction12).mult(pl1.mass).mult(-1)
+        const stop2Force = this.vectorProjection(pl2.velocity, direction21).mult(pl2.mass).mult(-1)
+        pl1.applyForce(stop1Force)
+        pl2.applyForce(stop2Force)
+        pl1.applyForce(resImpulse1)
+        pl2.applyForce(resImpulse2)
+    }
+
+    static wallCollide(pl){
+        if (pl.position.x - pl.radius <= 0 || pl.position.x + pl.radius >= window.innerWidth){
+            pl.velocity.x *= -0.7
+        }
+        if (pl.position.y - pl.radius <= 0 || pl.position.y + pl.radius >= window.innerHeight)
+            pl.velocity.y *= -0.7
+        
+        if (pl.position.x - pl.radius <= 0)
+            pl.position.x=0+pl.radius
+        if (pl.position.x + pl.radius >= window.innerWidth)
+            pl.position.x=window.innerWidth-pl.radius
+        if (pl.position.y - pl.radius <= 0)
+            pl.position.y=0+pl.radius
+        if (pl.position.y + pl.radius >= window.innerHeight)
+            pl.position.y=window.innerHeight-pl.radius
+       // pl.position.x = constrain(pl.position.x, pl.radius + 1, window.innerWidth - pl.radius - 1)
+        //pl.position.y = constrain(pl.position.y, pl.radius + 1, window.innerHeight - pl.radius - 1)
+    }
+}
